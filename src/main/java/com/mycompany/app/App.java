2e45b11df6b897b81b59f6d7e7ab3b0bcabb18af
zeynepyavuz1621 <zeynepyavuz1621@gmail.com>
2024-03-08 19:10:08 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,18 @@
 package com.mycompany.app;
 
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+
 /**
  * Hello world!
  *
@@ -44,6 +57,58 @@ public class App
 	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
-}
+
+    	 port(getHerokuAssignedPort());
+
+         get("/", (req, res) -> "Hello, World");
+
+         post("/compute", (req, res) -> {
+        	    // Parse the nameFormula parameter from the HTTP request
+        	    String[] nameFormulaStrings = req.queryParams("nameFormula").split(",");
+        	    Integer[] nameFormula = new Integer[nameFormulaStrings.length];
+        	    for (int i = 0; i < nameFormulaStrings.length; i++) {
+        	        nameFormula[i] = Integer.parseInt(nameFormulaStrings[i]);
+        	    }
+
+        	    // Parse the surnameFormula parameter from the HTTP request
+        	    String[] surnameFormulaStrings = req.queryParams("surnameFormula").split(",");
+        	    Integer[] surnameFormula = new Integer[surnameFormulaStrings.length];
+        	    for (int i = 0; i < surnameFormulaStrings.length; i++) {
+        	        surnameFormula[i] = Integer.parseInt(surnameFormulaStrings[i]);
+        	    }
+
+        	    // Get the name and surname parameters from the HTTP request
+        	    String name = req.queryParams("name");
+        	    String surname = req.queryParams("surname");
+
+        	    // Call the encryptor method
+        	    String[] result = App.encryptor(nameFormula, surnameFormula, name, surname);
+
+        	    // Prepare the result for the response
+        	    Map map = new HashMap();
+        	    map.put("result", result);
+        	    return new ModelAndView(map, "compute.mustache");
+         }, new MustacheTemplateEngine());
+
+
+         get("/compute",
+             (rq, rs) -> {
+               Map map = new HashMap();
+               map.put("result", "not computed yet!");
+               return new ModelAndView(map, "compute.mustache");
+             },
+             new MustacheTemplateEngine());
+     }
+
+     static int getHerokuAssignedPort() {
+         ProcessBuilder processBuilder = new ProcessBuilder();
+         if (processBuilder.environment().get("PORT") != null) {
+             return Integer.parseInt(processBuilder.environment().get("PORT"));
+         }
+         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+     }
+ }
+
+
+
+
